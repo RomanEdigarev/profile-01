5c19723a5a43332d19abc44040ff688b04321d93
RomanEdigarev <romzes2_09@mail.ru>
2020-10-17 20:05:16 +0300

    create and style header

---
+++ src/components/Header/ProfileMenu/ProfileName.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+import avatar from '../../../img/avatar.png'
+
+type PropsType = {
+    name: string
+}
+const ProfileName : FC<PropsType> = ({name}) => {
+    return (
+        <div className={'profile-name'}>
+            <img className={'profile-name__avatar'} src={avatar} alt=""/>
+            <span className="profile-name__name">{name}</span>
+        </div>
+    );
+};
+
+export default ProfileName;
